6b852661293cde686a6387e1316499f4b0a0d8e9
arlenz <arlenz@192.168.15.5>
2017-07-09 23:34:56 -0300

    First commit: Create Project funregulation - Netbeans IDE 8.2 - Python 3.6 - funregulation_promoterprediction.py (genbank and P.echinulatum) - funregulation.py (menu)

---
+++ bin/funregulation-promoterpredict.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import inspect
+import shutil
+import argparse
+import urllib.parse
+import lib.library as lib
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.Alphabet import generic_dna
+from Bio.SeqRecord import SeqRecord
+from collections import namedtuple
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+
+#setup menu with argparse
+class MyFormatter(argparse.ArgumentDefaultsHelpFormatter):
+    def __init__(self, prog):
+        super(MyFormatter, self).__init__(prog, max_help_position=48)
+parser = argparse.ArgumentParser(prog='funregulation-promoterpredict.py', usage="%(prog)s [options] -g genome.fasta -a annotation.gff3 -o /output_folder",
+    description = '''Script that does it all.''',
+    epilog = "Written by Alexandre Lenz (2017-2019) arlenz@ucs.br / alenz@uneb.br",
+    formatter_class = MyFormatter)
+parser.add_argument('-g', '--genome', required=False, help='Genome in FASTA format')
+parser.add_argument('-a', '--annotation', required=False, help='Annotation in GFF3 format')
+parser.add_argument('-o', '--out', required=False, help='Output folder')
+args = parser.parse_args()
+
+#testing without args
+args.out = '/Users/arlenz/NetBeansProjects/funregulation/output/'
+args.genome = '/Users/arlenz/NetBeansProjects/funregulation/input/P.oxalicum114-2.fasta'
+args.annotation = '/Users/arlenz/NetBeansProjects/funregulation/input/P.oxalicum114-2.gff'
+
+#create folder structure
+if not os.path.isdir(args.out):
+    os.makedirs(args.out)
+    os.makedirs(os.path.join(args.out, 'predict_misc'))
+    os.makedirs(os.path.join(args.out, 'predict_results'))
+    os.makedirs(os.path.join(args.out, 'logfiles'))
+else:
+    if os.path.isdir(os.path.join(args.out, 'predict_results')):
+        shutil.rmtree(os.path.join(args.out, 'predict_results'))
+        os.makedirs(os.path.join(args.out, 'predict_results'))
+    #make sure subdirectories exist
+    dirs = [os.path.join(args.out, 'predict_misc'), os.path.join(args.out, 'predict_results'), os.path.join(args.out, 'logfiles')]
+    for d in dirs:
+        if not os.path.isdir(d):
+            os.makedirs(d)
+        
+#create log file
+log_name = os.path.join(args.out, 'logfiles', 'funregulation-promoterpredict.log')
+if os.path.isfile(log_name):
+    os.remove(log_name)
+
+#initialize script, log system info and cmd issue at runtime
+lib.setupLogging(log_name)
+FNULL = open(os.devnull, 'w')
+cmd_args = " ".join(sys.argv)+'\n'
+lib.log.debug(cmd_args)
+#lib.SystemInfo()
+
+#get version of funregulation
+#version = lib.get_version()
+#lib.log.info("Running %s" % version)
+
+#check input files to make sure they are not empty
+input_checks = [args.genome, args.annotation]
+for i in input_checks:
+    if i:
+        lib.checkinputs(i)
+
+# promoter lenght definition
+upstream = -300
+downstream = +40
+
+# Initialized GeneInfo named tuple to handle with GFF3 annotation. Note: namedtuple is immutable
+gffInfoFields = ["seqid", "source", "ltype", "start", "end", "score", "strand", "phase", "attributes"]
+GFFRecord = namedtuple("GFFRecord", gffInfoFields)
+
+# Initialized PromoterInfo named tuple to handle with Promoter BED file. Note: namedtuple is immutable
+bedInfoFields = ["gene", "strand", "scaffold", "start", "stop"]
+BEDRecord = namedtuple("BEDRecord", bedInfoFields)
+
+"""Parse the GFF3 attribute column and return a dict"""
+def parse_gff_attributes(attributeString):
+    if attributeString == ".": return {}
+    ret ={}
+    if ";" not in attributeString: 
+        ret=attributeString
+    if ";" in attributeString:
+        for attribute in attributeString.split(";"):
+            key, value = attribute.split("=")
+            ret[urllib.parse.unquote(key)] = urllib.parse.unquote(value)
+    return ret
+
+"""
+    A minimalistic GFF3 format parser.
+    Yields objects that contain info about a single GFF3 feature.
+"""
+def parse_gff3(filename):
+    lib.log.info("Parsing GFF3 File")
+    with open(filename) as infile:
+        for line in infile:
+            if line.startswith("#"): continue
+            parts = line.strip().split("\t")
+            #If this fails, the file format is not standard-compatible
+            assert len(parts) == len(gffInfoFields)
+            #Normalize data
+            normalizedInfo = {
+                "seqid": None if parts[0] == "." else urllib.parse.unquote(parts[0]),
+                "source": None if parts[1] == "." else urllib.parse.unquote(parts[1]),
+                "ltype": None if parts[2] == "." else urllib.parse.unquote(parts[2]),
+                "start": None if parts[3] == "." else int(parts[3]),
+                "end": None if parts[4] == "." else int(parts[4]),
+                "score": None if parts[5] == "." else float(parts[5]),
+                "strand": None if parts[6] == "." else urllib.parse.unquote(parts[6]),
+                "phase": None if parts[7] == "." else urllib.parse.unquote(parts[7]),
+                "attributes": parse_gff_attributes(parts[8])
+            }
+            #Alternatively, you can emit the dictionary here, if you need mutability:
+            #    yield normalizedInfo
+            yield GFFRecord(**normalizedInfo)
+    lib.log.info("GFF3 File parsed correctly")
+    
+    
+"""
+Write promoters data (gene, scaffold, strand and start codon postions) to new bed file
+ 
+"""
+def create_promoters_bed_file(file_promoters_bed):
+    lib.log.info("Start promoter prediction")
+    recordCount = 0
+    promoters_partially_extracted = 0
+    
+    if os.path.isfile(file_promoters_bed):
+        lib.log.info("File does exist and will be overwritten")
+    else:
+        lib.log.info("No such file. New promoter prediction file will be created")
+        f = open(os.path.join(args.out, file_promoters_bed), 'w')
+        f.write('')
+        f.close()
+        
+    
+    with open(file_promoters_bed, 'w') as outfp:
+        for record in parse_gff3(args.annotation):
+            if record.ltype == 'gene':
+                #Access attributes like this: my_strand = record.strand
+                gene = str(record.attributes)
+                if record.strand == '+':
+                    if record.start+upstream > 0:
+                        outfp.write(gene+'\t'+record.strand+'\t'+record.seqid+'\t'+str(record.start+upstream)+'\t'+str(record.start+downstream+2)+'\n')
+                    else:    
+                        # incomplete promoters
+                        outfp.write(gene+'\t'+record.strand+'\t'+record.seqid+'\t'+str(1)+'\t'+str(record.start+downstream+2)+'\n')
+                        lib.log.info("Promoter from gene " + gene + " can't be fully extracted")
+                        promoters_partially_extracted += 1
+                else:
+                    if record.end+upstream > 0:
+                        outfp.write(gene+'\t'+record.strand+'\t'+record.seqid+'\t'+str(record.end+upstream)+'\t'+str(record.end+downstream-2)+'\n')
+                    else:    
+                        # incomplete promoters
+                        outfp.write(gene+'\t'+record.strand+'\t'+record.seqid+'\t'+str(1)+'\t'+str(record.end+downstream-2)+'\n')
+                        lib.log.info("Promoter from gene " + gene + " can't be fully extracted")
+                        promoters_partially_extracted += 1
+                recordCount += 1
+    lib.log.info("%d promoters were found" % recordCount)
+    lib.log.info("Promoters partially extracted: %d" % promoters_partially_extracted)
+    lib.log.info("Promoter prediction successful")
+    
+"""
+    A minimalistic GFF3 format parser.
+    Yields objects that contain info about a single GFF3 feature.
+    
+"""
+def parse_bed(file_promoters_bed):
+    lib.log.info("Parsing BED File")
+
+    with open(file_promoters_bed) as infile:
+        for line in infile:
+            if line.startswith("#"): continue
+            parts = line.strip().split("\t")
+            #If this fails, the file format is not standard-compatible
+            assert len(parts) == len(bedInfoFields)
+            #Normalize data
+            normalizedInfo = {
+                "gene": None if parts[0] == "." else urllib.parse.unquote(parts[0]),
+                "strand": None if parts[1] == "." else urllib.parse.unquote(parts[1]),
+                "scaffold": None if parts[2] == "." else urllib.parse.unquote(parts[2]),
+                "start": None if parts[3] == "." else int(parts[3]),
+                "stop": None if parts[4] == "." else int(parts[4])
+            }
+            #Alternatively, you can emit the dictionary here, if you need mutability:
+            #    yield normalizedInfo
+            yield BEDRecord(**normalizedInfo)
+    lib.log.info("BED File parsed correctly")
+
+"""
+Reads promoters data (genes, scaffolds and postions) from bed file 
+and extract each gene promoter to new fasta file
+ 
+"""
+def extract_promoters_from_genome_fasta_file(file_promoters_bed):
+    lib.log.info("Start promoters extraction from genome")
+    promoters_partially_extracted = 0
+    # parse fasta file and turn into dictionary
+    records = SeqIO.to_dict(SeqIO.parse(open(args.genome), 'fasta'))
+
+    for gene in parse_bed(file_promoters_bed):
+        long_seq_record = records[gene.scaffold]
+        long_seq = long_seq_record.seq
+        alphabet = long_seq.alphabet
+        short_seq = str(long_seq)[gene.start:gene.stop]
+        if gene.strand == '-':
+            my_dna = Seq(short_seq, generic_dna)
+            my_dna.reverse_complement()
+            my_dna.complement()
+            short_seq=str(my_dna)
+            if gene.stop > len(my_dna)+gene.start :
+                lib.log.info("Promoter from gene " + gene.gene + " can't be fully extracted")
+                promoters_partially_extracted += 1
+                
+        short_seq_record = SeqRecord(Seq(short_seq, alphabet), id=gene.gene, description=gene.scaffold+'_'+str(gene.start)+':'+str(gene.stop))
+        with open(args.out+'predict_misc/'+gene.gene+'_promoter.fasta', 'w') as f:
+            SeqIO.write(short_seq_record, f, 'fasta')
+    lib.log.info("Promoters partially extracted: %d" % promoters_partially_extracted)
+    lib.log.info("Promoters extraction succesful")
+    lib.log.info("Promoters in"+args.out+'predict_misc')
+    lib.log.info("funregulation Promoter Prediction Successful")
+    sys.exit(0)
+    
+
+# file definitions
+file_promoters_bed = os.path.join(args.out,'predict_results','promoters.bed')
+#Execute the promoters BED file creator
+create_promoters_bed_file (file_promoters_bed)
+#Execute the promoter extraction from Fasta genome
+extract_promoters_from_genome_fasta_file(file_promoters_bed)
---
+++ funregulation.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+#Wrapper script for Funannotate package.
+
+import sys
+import os
+import subprocess
+import inspect
+
+script_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+
+def flatten(l):
+    flatList = []
+    for elem in l:
+        # if an element of a list is a list
+        # iterate over this list and add elements to flatList 
+        if type(elem) == list:
+            for e in elem:
+                flatList.append(e)
+        else:
+            flatList.append(elem)
+    return flatList
+
+version = '0.0.1'
+
+default_help = """
+Usage:       funregulation <command> <arguments>
+version:     %s
+Description: Funregulation is a promoter and transcription binding site prediction, annotation, and comparison pipeline.
+    
+Command:     promoterpredict     Run promoter prediction pipeline
+             #tbspredict         Run transcription binding site prediction pipeline
+             promoterannotate    Assign annotation to elements of promoter predictions
+             #tbsannotate        Assign annotation to elements of transcription binding site predictions
+             #compare            Compare funregulation genomes
+             
+Written by Alexandre Lenz (2017-2019) arlenz@ucs.br / alenz@uneb.br
+        """ % version
+
+if len(sys.argv) > 1:
+    
+    if sys.argv[1] == 'promoterpredict':
+        help = """
+Usage:       funregulation %s <arguments>
+version:     %s
+Description: Script takes genome multi-fasta file and GFF3 annotation file to do a whole
+             genome promoter prediction using the start_codon. 
+    
+Required:  -g, --genome           Genome multi-fasta file.
+           -a, --annotation       GFF3 annotation file.
+           -o, --out              Output folder name.
+            
+Written by Alexandre Lenz (2017-2019) arlenz@ucs.br / alenz@uneb.br
+        """ % (sys.argv[1], version)
+        
+        arguments = sys.argv[2:]
+        if len(arguments) > 1:
+            cmd = os.path.join(script_path, 'bin', 'funregulation-promoterpredict.py')
+            arguments.insert(0, cmd)
+            exe = sys.executable
+            arguments.insert(0, exe)
+            subprocess.call(arguments)
+        else:
+            print (help)
+            sys.exit(1)
+    elif sys.argv[1] == 'promoterannotate':
+        help = """
+Usage:       funregulation %s <arguments>
+version:     %s
+Description: Script annotates the elements from funregulation promoterpredict.  It pulls
+             annotation info using  
+             ElemeNT: A Computational Tool for Detecting Core Promoter Elements
+             http://www.ncbi.nlm.nih.gov/pubmed/26226151
+
+Required:    -i, --input        Folder from funregulation promoter predict
+          
+Written by Alexandre Lenz (2017-2019) arlenz@ucs.br / alenz@uneb.br
+        """ % (sys.argv[1], version)
+        
+    elif sys.argv[1] == 'compare':
+        help = """
+Usage:       funregulation %s <arguments>
+version:     %s
+Description: Script does comparative regulatory elements between funregulation genomes.  Output
+             is graphs, CSV files --> visualized in web-browser.  
+    
+Required:    -i, --input         List of funregulation genome folders
+Optional:    -o, --out           Output folder name. Default: funregulation_compare
+             
+Written by Alexandre Lenz (2017-2019) arlenz@ucs.br / alenz@uneb.br
+        """ % (sys.argv[1], version)
+       
+        arguments = sys.argv[2:]
+        if len(arguments) > 1:
+            cmd = os.path.join(script_path, 'bin', 'funregulation-compare.py')
+            arguments.insert(0, cmd)
+            exe = sys.executable
+            arguments.insert(0, exe)
+            subprocess.call(arguments)
+        else:
+            print (help)
+            sys.exit(1)
+else:
+    print (default_help)
